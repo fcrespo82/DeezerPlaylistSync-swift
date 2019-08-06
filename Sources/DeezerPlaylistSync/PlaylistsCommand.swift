eb57d6d2aca1c93a4afc3be54161d93b73bead38
Fernando Crespo <fernando82@gmail.com>
2019-08-06 19:04:43 -0300

    Add playlist

--- Sources/DeezerPlaylistSync/PlaylistsCommand.swift
+++ Sources/DeezerPlaylistSync/PlaylistsCommand.swift
@@ -21,7 +21,17 @@ struct PlaylistsCommand: Command {
   func run(using context: CommandContext) throws -> Future<Void> {
     let user = try context.argument("user")
     let verbose = context.options["verbose"] ?? "false"
-    context.console.print("teste \(user) \(verbose)")
+
+    guard let token = DeezerToken.forUser(user) else {
+      print("Cannot find token for \(user)")
+      exit(1)
+    }
+    let client = DeezerClient(withToken: token)
+
+    context.console.print("\nPlaylists from \(user)")
+    _ = try client.playlists().map { playlist in
+      print("\(playlist.title) \t - \(playlist.nb_tracks) tracks")
+    }
 
     return .done(on: context.container)
   }
--- Tests/DeezerPlaylistSyncTests/DeezerPlaylistSyncTests.swift
+++ Tests/DeezerPlaylistSyncTests/DeezerPlaylistSyncTests.swift
@@ -1,47 +1,31 @@
-import XCTest
 import class Foundation.Bundle
+import XCTest
+import class DeezerPlaylistSync.DeezerToken
 
 final class DeezerPlaylistSyncTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+  func loadToken() throws {
+    let name = "fernando"
+    let token = DeezerToken.forUser(name)
+    // guard let token = token else {
+    //   print("Cannot find token for \(name)")
+    //   exit(1)
+    // }
+    XCTAssertNotNil(token, "Token should be loaded")
+  }
 
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
+  /// Returns path to the built products directory.
+  var productsDirectory: URL {
+    #if os(macOS)
+      for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+        return bundle.bundleURL.deletingLastPathComponent()
+      }
+      fatalError("couldn't find the products directory")
+    #else
+      return Bundle.main.bundleURL
+    #endif
+  }
 
-        let fooBinary = productsDirectory.appendingPathComponent("DeezerPlaylistSync")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
-    }
-
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  static var allTests = [
+    ("loadToken", loadToken),
+  ]
 }
